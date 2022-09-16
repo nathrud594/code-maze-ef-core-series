02fbe8d242466d4312e21791273f5f431d06128a
nathrud594 <nathrud594@gmail.com>
2022-09-16 18:01:06 +0530

    completed individual migration

---
+++ EFCoreApp/EFCoreApp/Migrations/20220916064759_initialcreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreApp.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE MyCustomProcedure
+                               AS
+                               SELECT * FROM Student");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.Sql(@"DROP PROCEDURE MyCustomProcedure");
+        
+        }
+    }
+}
